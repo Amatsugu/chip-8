a87d533e28a98158090371d958a567d6c8314400
Amatsugu <khamraj@kaisei.app>
2025-03-10 23:03:19 -0400

    First few instructions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+pub mod chip8;
+pub mod tests;
+
+use std::{env, fs};
+
+use chip8::Chip8;
+
 fn main() {
-    println!("Hello, world!");
+	let args: Vec<String> = env::args().collect();
+	if args.len() < 2 {
+		println!("No file provided");
+		return;
+	}
+	let path = &args[1];
+	let file = fs::read(path);
+
+	let bytes = file.expect("Failed to read file");
+
+	let mut emu = Chip8::new();
+	emu.load_code(bytes);
+	emu.tick();
+
+	println!("PC: {}", emu.program_counter);
 }
